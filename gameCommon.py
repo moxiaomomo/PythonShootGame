bee1d22019e97bd77174a5eef278cca53056f081
moxiaomomo <moxiaomomo@gmail.com>
2013-10-07 14:51:21 +0800

    devide the main module and add  code into functions

---
+++ gameCommon.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 07 11:05:00 2013
+
+@author: xiaomo
+"""
+
+import pygame
+from sys import exit
+from pygame.locals import *
+from gameRole import *
+import random
+
+# 初始化游戏
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('飞机大战')
+
+# 载入游戏音乐
+bullet_sound = pygame.mixer.Sound('resources/sound/bullet.wav')
+enemy1_down_sound = pygame.mixer.Sound('resources/sound/enemy1_down.wav')
+game_over_sound = pygame.mixer.Sound('resources/sound/game_over.wav')
+bullet_sound.set_volume(0.3)
+enemy1_down_sound.set_volume(0.3)
+game_over_sound.set_volume(0.3)
+pygame.mixer.music.load('resources/sound/game_music.wav')
+pygame.mixer.music.play(-1, 0.0)
+pygame.mixer.music.set_volume(0.25)
+
+# 载入背景图
+IMG_BACKGROUND = pygame.image.load('resources/image/background.png').convert()
+img_game_over = pygame.image.load('resources/image/gameover.png')
+
+filename = 'resources/image/shoot.png'
+plane_img = pygame.image.load(filename)
+
+# 设置玩家相关参数
+player_rect = []
+player_rect.append(pygame.Rect(0, 99, 102, 126))        # 玩家精灵图片区域
+player_rect.append(pygame.Rect(165, 360, 102, 126))
+player_rect.append(pygame.Rect(165, 234, 102, 126))     # 玩家爆炸精灵图片区域
+player_rect.append(pygame.Rect(330, 624, 102, 126))
+player_rect.append(pygame.Rect(330, 498, 102, 126))
+player_rect.append(pygame.Rect(432, 624, 102, 126))
+player_pos = [200, 500]
+player = Player(plane_img, player_rect, player_pos)
+
+# 定义子弹对象使用的surface相关参数
+bullet_rect = pygame.Rect(1004, 987, 9, 21)
+bullet_img = plane_img.subsurface(bullet_rect)
+
+# 定义敌机对象使用的surface相关参数
+enemy1_rect = pygame.Rect(534, 612, 57, 43)
+enemy1_img = plane_img.subsurface(enemy1_rect)
+enemy1_down_imgs = []
+enemy1_down_imgs.append(plane_img.subsurface(pygame.Rect(267, 347, 57, 43)))
+enemy1_down_imgs.append(plane_img.subsurface(pygame.Rect(873, 697, 57, 43)))
+enemy1_down_imgs.append(plane_img.subsurface(pygame.Rect(267, 296, 57, 43)))
+enemy1_down_imgs.append(plane_img.subsurface(pygame.Rect(930, 697, 57, 43)))
+
+enemies1 = pygame.sprite.Group()
+
+# 存储被击毁的飞机，用来渲染击毁精灵动画
+enemies_down = pygame.sprite.Group()
